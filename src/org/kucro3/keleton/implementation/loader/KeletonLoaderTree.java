b1c3056d0e9890eca8aa06d3d25b022686faaadd
Kumonda221 <kumonda@foxmail.com>
2017-12-30 10:37:11 +0800

    增量更新 20171230

--- src/org/kucro3/keleton/implementation/loader/KeletonLoaderTree.java
+++ src/org/kucro3/keleton/implementation/loader/KeletonLoaderTree.java
@@ -1,5 +1,7 @@
 package org.kucro3.keleton.implementation.loader;
 
+import com.sun.media.sound.RIFFInvalidDataException;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,6 +16,8 @@ class KeletonLoaderTree {
 
     private Node tail;
 
+    private int modCount;
+
     class Node
     {
         Node(KeletonLoadedModule module)
@@ -27,7 +31,10 @@ class KeletonLoaderTree {
                 prev.next = next;
 
             if(next != null)
+            {
                 next.prev = prev;
+                next.index.index = prev == null ? new RelatedIndex(new FixedIndex(0)) : prev.index;
+            }
 
             mapped.remove(module.getModule().name());
 
@@ -40,12 +47,21 @@ class KeletonLoaderTree {
         {
             if(tail == null)
                 tail = this;
+
             if(node == null)
+            {
                 head = this;
+                index = new RelatedIndex(new FixedIndex(0));
+            }
             else
             {
+                index = new RelatedIndex(node.index);
+
                 if(node.next != null)
+                {
+                    node.next.index.index = index;
                     node.next.prev = this;
+                }
 
                 prev = node;
                 next = node.next;
@@ -57,12 +73,23 @@ class KeletonLoaderTree {
         {
             if(head == null)
                 head = this;
+
             if(node == null)
+            {
                 tail = this;
+                index = new RelatedIndex(new FixedIndex(0));
+            }
             else
             {
                 if(node.prev != null)
+                {
+                    index = new RelatedIndex(node.prev.index);
                     node.prev.next = this;
+                }
+                else
+                    index = new RelatedIndex(new FixedIndex(0));
+
+                node.index.index = index;
 
                 prev = node.prev;
                 next = node;
@@ -70,10 +97,62 @@ class KeletonLoaderTree {
             }
         }
 
+        RelatedIndex index;
+
         KeletonLoadedModule module;
 
         Node prev;
 
         Node next;
+    }
+
+    interface Index
+    {
+        int get();
+    }
+
+    class RelatedIndex implements Index
+    {
+        RelatedIndex(Index index)
+        {
+            this.index = index;
+            this.computed = compute();
+        }
+
+        int compute()
+        {
+            this.lastModCount = modCount;
+            return index.get() + 1;
+        }
+
+        @Override
+        public int get()
+        {
+            if(lastModCount != modCount)
+                compute();
+            return computed;
+        }
+
+        int lastModCount;
+
+        int computed;
+
+        Index index;
+    }
+
+    class FixedIndex implements Index
+    {
+        FixedIndex(int index)
+        {
+            this.index = index;
+        }
+
+        @Override
+        public int get()
+        {
+            return this.index;
+        }
+
+        int index;
     }
 }
