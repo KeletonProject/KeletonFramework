4b4cd5e7cae2f9ae7ab58348325e94978d62279a
Kumonda221 <kumonda@foxmail.com>
2018-01-08 12:34:26 +0800

    增量更新 20180108

--- src/org/kucro3/keleton/world/home/event/HomeClearEvent.java
+++ src/org/kucro3/keleton/world/home/event/HomeClearEvent.java
@@ -1,21 +1,49 @@
 package org.kucro3.keleton.world.home.event;
 
-import org.kucro3.keleton.event.ProceduralEvent;
+import org.kucro3.keleton.event.CancellableWithCause;
 import org.spongepowered.api.world.World;
 
 import java.util.UUID;
 
-public interface HomeClearEvent extends ProceduralEvent {
+public interface HomeClearEvent extends HomeCollectionEvent {
     public interface ByWorld
     {
+        public interface Pre extends ByWorld, HomeCollectionEvent.Pre, CancellableWithCause
+        {
+        }
 
+        public interface Cancelled extends ByWorld, HomeCollectionEvent.Cancelled
+        {
+        }
+
+        public interface Completed extends ByWorld, HomeCollectionEvent.Completed
+        {
+        }
+
+        public interface Failed extends ByWorld, HomeCollectionEvent.Failed
+        {
+        }
 
         World getWorld();
     }
 
     public interface ByOwner
     {
+        public interface Pre extends ByOwner, HomeCollectionEvent.Pre, CancellableWithCause
+        {
+        }
 
+        public interface Cancelled extends ByOwner, HomeCollectionEvent.Cancelled
+        {
+        }
+
+        public interface Completed extends ByOwner, HomeCollectionEvent.Completed
+        {
+        }
+
+        public interface Failed extends ByOwner, HomeCollectionEvent.Failed
+        {
+        }
 
         UUID getOwnerUniqueId();
     }
--- src/org/kucro3/keleton/world/home/event/HomeCollectionEvent.java
+++ src/org/kucro3/keleton/world/home/event/HomeCollectionEvent.java
@@ -1,8 +1,25 @@
 package org.kucro3.keleton.world.home.event;
 
+import org.kucro3.keleton.event.CancellableWithCause;
 import org.kucro3.keleton.event.ProceduralEvent;
 import org.kucro3.keleton.world.home.HomeCollection;
 
 public interface HomeCollectionEvent extends ProceduralEvent {
+    public interface Pre extends HomeCollectionEvent, ProceduralEvent.Pre, CancellableWithCause
+    {
+    }
+
+    public interface Cancelled extends HomeCollectionEvent, ProceduralEvent.Cancelled
+    {
+    }
+
+    public interface Completed extends HomeCollectionEvent, ProceduralEvent.Completed
+    {
+    }
+
+    public interface Failed extends HomeCollectionEvent, ProceduralEvent.Failed
+    {
+    }
+
     public HomeCollection getCollection();
 }
