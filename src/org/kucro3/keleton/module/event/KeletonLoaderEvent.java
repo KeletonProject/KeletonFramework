dcf30515ed807b0d91f892f404445fb86996e8b0
Kumonda221 <kumonda@foxmail.com>
2018-01-13 13:07:08 +0800

    增量更新 20180113

--- src/org/kucro3/keleton/module/event/KeletonLoaderEvent.java
+++ src/org/kucro3/keleton/module/event/KeletonLoaderEvent.java
@@ -4,29 +4,30 @@ import org.kucro3.keleton.event.CancellableWithCause;
 import org.kucro3.keleton.event.ProceduralEvent;
 import org.kucro3.keleton.module.KeletonInstance;
 import org.kucro3.keleton.module.KeletonModule;
+import org.kucro3.keleton.module.Module;
 
+import java.util.Optional;
 import java.util.Set;
 
 public interface KeletonLoaderEvent extends ProceduralEvent {
+    public Module getInfo();
+
     public interface Pre extends KeletonLoaderEvent, ProceduralEvent.Pre, CancellableWithCause
     {
-        public String getId();
-
-        public Set<String> getDependencies();
     }
 
     public interface Ignored extends KeletonLoaderEvent, ProceduralEvent.Failed
     {
-        public KeletonInstance getInstance();
-
         public String getMessage();
+    }
+
+    public interface Failed extends KeletonLoaderEvent, ProceduralEvent.Failed
+    {
+        public Optional<Exception> getException();
     }
 
     public interface Cancelled extends KeletonLoaderEvent, ProceduralEvent.Cancelled
     {
-        public String getId();
-
-        public Set<String> getDependencies();
     }
 
     public interface Discovered extends KeletonLoaderEvent, ProceduralEvent.Completed
